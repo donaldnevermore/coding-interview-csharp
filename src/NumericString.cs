e4690187267d3024f1b9c058687792bcab535c61
Dongcheng Wang <dongcheng6@qq.com>
2020-05-11 19:05:01 +0800

    Add NumericString

---
+++ src/NumericString.cs
@@ -0,0 +1,48 @@
+﻿// Interview 20
+
+namespace CodingInterview {
+    public class NumericString {
+        private int index = 0;
+
+        public bool IsNumeric(string str) {
+            if (str == null) {
+                return false;
+            }
+
+            bool numeric = ScanInteger(str);
+
+            if (index <= str.Length - 1 && str[index] == '.') {
+                index++;
+
+                // Numbers before and after '.' are optional.
+                numeric = ScanUnsignedInteger(str) || numeric;
+            }
+
+            if (index <= str.Length - 1 && (str[index] == 'e' || str[index] == 'E')) {
+                index++;
+
+                // Numbers before and after 'e' are obligatory.
+                numeric = numeric && ScanInteger(str);
+            }
+
+            return numeric && (index > str.Length - 1);
+        }
+
+        private bool ScanUnsignedInteger(string str) {
+            int before = index;
+            while (index <= str.Length - 1 && str[index] >= '0' && str[index] <= '9') {
+                index++;
+            }
+
+            return index > before;
+        }
+
+        private bool ScanInteger(string str) {
+            if (index <= str.Length - 1 && (str[index] == '-' || str[index] == '+')) {
+                index++;
+            }
+
+            return ScanUnsignedInteger(str);
+        }
+    }
+}
---
+++ src/NumericStringTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace CodingInterview {
+    [TestFixture]
+    public class NumericStringTest {
+        [Test]
+        public void TestIsNumeric() {
+            Assert.AreEqual(true, new NumericString().IsNumeric("+100"));
+            Assert.AreEqual(true, new NumericString().IsNumeric("5e2"));
+            Assert.AreEqual(true, new NumericString().IsNumeric("-123"));
+            Assert.AreEqual(true, new NumericString().IsNumeric("3.1416"));
+            Assert.AreEqual(true, new NumericString().IsNumeric("-1E-6"));
+
+            Assert.AreEqual(false, new NumericString().IsNumeric("12e"));
+            Assert.AreEqual(false, new NumericString().IsNumeric("1a3.14"));
+            Assert.AreEqual(false, new NumericString().IsNumeric("1.2.3"));
+            Assert.AreEqual(false, new NumericString().IsNumeric("+-5"));
+            Assert.AreEqual(false, new NumericString().IsNumeric("12e+5.4"));
+        }
+    }
+}
