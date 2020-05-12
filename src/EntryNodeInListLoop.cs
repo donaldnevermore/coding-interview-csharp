3078fa74a052621c303a9e5a118a9f5fd1de5d97
Dongcheng Wang <dongcheng6@qq.com>
2020-05-12 22:40:46 +0800

    Add EntryNodeInListLoop

---
+++ src/EntryNodeInListLoop.cs
@@ -0,0 +1,60 @@
+﻿// Interview 23
+
+namespace CodingInterview {
+    public class EntryNodeInListLoop {
+        public static ListNode EntryNodeOfLoop(ListNode head) {
+            ListNode meetingNode = MeetingNode(head);
+            if (meetingNode == null) {
+                return null;
+            }
+
+            ListNode countingNode = meetingNode;
+            int nodesInLoop = 1;
+            while (countingNode.Next != meetingNode) {
+                countingNode = countingNode.Next;
+                nodesInLoop++;
+            }
+
+            ListNode ahead = head;
+            for (int i = 0; i < nodesInLoop; i++) {
+                ahead = ahead.Next;
+            }
+
+            ListNode behind = head;
+            while (ahead != behind) {
+                ahead = ahead.Next;
+                behind = behind.Next;
+            }
+
+            return ahead;
+        }
+
+        private static ListNode MeetingNode(ListNode head) {
+            if (head == null) {
+                return null;
+            }
+
+            ListNode slow = head.Next;
+            if (slow == null) {
+                return null;
+            }
+
+            ListNode fast = slow.Next;
+
+            while (fast != null && slow != null) {
+                if (fast == slow) {
+                    return fast;
+                }
+
+                slow = slow.Next;
+
+                fast = fast.Next;
+                if (fast != null) {
+                    fast = fast.Next;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ src/EntryNodeInListLoopTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace CodingInterview {
+    [TestFixture]
+    public class EntryNodeInListLoopTest {
+        [Test]
+        public void TestEntryOfLoop() {
+            var node1 = new ListNode {Value = 1};
+            var node2 = new ListNode {Value = 2};
+            node1.Next = node2;
+            var node3 = new ListNode {Value = 3};
+            node2.Next = node3;
+            node3.Next = node2;
+
+            Assert.AreEqual(node2, EntryNodeInListLoop.EntryNodeOfLoop(node1));
+        }
+
+        [Test]
+        public void TestNoEntryOfLoop() {
+            var node1 = new ListNode {Value = 1};
+            Assert.AreEqual(null, EntryNodeInListLoop.EntryNodeOfLoop(node1));
+
+            var node2 = new ListNode {Value = 2};
+            node1.Next = node2;
+            Assert.AreEqual(null, EntryNodeInListLoop.EntryNodeOfLoop(node1));
+        }
+    }
+}
