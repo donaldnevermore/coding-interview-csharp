9fc3e21f0809f011f26798317df09f33196026bd
Dongcheng Wang <dongcheng6@qq.com>
2020-05-12 18:48:26 +0800

    Add KthNodeFromEnd

---
+++ src/KthNodeFromEnd.cs
@@ -0,0 +1,30 @@
+﻿// Interview 22
+
+namespace CodingInterview {
+    public class KthNodeFromEnd {
+        public static ListNode FindKthToTail(ListNode head, uint k) {
+            if (head == null || k == 0) {
+                return null;
+            }
+
+            ListNode ahead = head;
+
+            for (int i = 0; i < k - 1; i++) {
+                if (ahead.Next != null) {
+                    ahead = ahead.Next;
+                }
+                else {
+                    return null;
+                }
+            }
+
+            ListNode behind = head;
+            while (ahead.Next != null) {
+                ahead = ahead.Next;
+                behind = behind.Next;
+            }
+
+            return behind;
+        }
+    }
+}
