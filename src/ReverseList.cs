c6925fbee929e8aeae6efcbc5eb159bf8cf30ea8
Dongcheng Wang <dongcheng6@qq.com>
2020-05-13 19:27:21 +0800

    Add ReverseList

---
+++ src/ReverseList.cs
@@ -0,0 +1,26 @@
+﻿// Interview 24
+
+namespace CodingInterview {
+    public class ReverseList {
+        public static ListNode Reverse(ListNode head) {
+            ListNode reversedHead = null;
+            ListNode node = head;
+            ListNode previous = null;
+
+            while (node != null) {
+                ListNode next = node.Next;
+
+                if (next == null) {
+                    reversedHead = node;
+                }
+
+                node.Next = previous;
+
+                previous = node;
+                node = next;
+            }
+
+            return reversedHead;
+        }
+    }
+}
---
+++ src/ReverseListTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace CodingInterview {
+    [TestFixture]
+    public class ReverseListTest {
+        [Test]
+        public void TestReverse() {
+            Assert.AreEqual(null, ReverseList.Reverse(null));
+
+            var node1 = new ListNode {Value = 1};
+            Assert.AreEqual(node1, ReverseList.Reverse(node1));
+
+            var node2 = new ListNode {Value = 2};
+            node1.Next = node2;
+            Assert.AreEqual(node2, ReverseList.Reverse(node1));
+        }
+    }
+}
