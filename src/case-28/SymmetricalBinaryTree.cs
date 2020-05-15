e4006c76fc1b33d67147cb0cebba5d87a3d28bd2
Dongcheng Wang <dongcheng6@qq.com>
2020-05-15 21:57:18 +0800

    Add SymmetricalBinaryTree

---
+++ src/case-28/SymmetricalBinaryTree.cs
@@ -0,0 +1,23 @@
+﻿namespace CodingInterview {
+    public class SymmetricalBinaryTree {
+        public static bool IsSymmetrical(BinaryTreeNode root) {
+            return IsSymmetrical(root, root);
+        }
+
+        private static bool IsSymmetrical(BinaryTreeNode root1, BinaryTreeNode root2) {
+            if (root1==null &&root2==null) {
+                return true;
+            }
+
+            if (root1==null || root2 ==null) {
+                return false;
+            }
+
+            if (root1.Value!=root2.Value) {
+                return false;
+            }
+
+            return IsSymmetrical(root1.Left, root2.Right) && IsSymmetrical(root1.Right, root2.Left);
+        }
+    }
+}
