e7d96255a769c07ca353f4ea71f8d3cee927a253
Dongcheng Wang <dongcheng6@qq.com>
2020-05-15 11:09:49 +0800

    Add MirrorOfBinaryTree

---
+++ src/case-27/MirrorOfBinaryTree.cs
@@ -0,0 +1,25 @@
+﻿namespace CodingInterview {
+    public class MirrorOfBinaryTree {
+        public static void Mirror(BinaryTreeNode node) {
+            if (node == null) {
+                return;
+            }
+
+            if (node.Left == null && node.Right == null) {
+                return;
+            }
+
+            BinaryTreeNode temp = node.Left;
+            node.Left = node.Right;
+            node.Right = temp;
+
+            if (node.Left != null) {
+                Mirror(node.Left);
+            }
+
+            if (node.Right != null) {
+                Mirror(node.Right);
+            }
+        }
+    }
+}
