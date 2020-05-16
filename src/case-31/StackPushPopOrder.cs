ef4560a8111f16387a1dea5bb23ce32ee7178d40
Dongcheng Wang <dongcheng6@qq.com>
2020-05-16 20:20:04 +0800

    Add StackPushPopOrder

---
+++ src/case-31/StackPushPopOrder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodingInterview {
+    public class StackPushPopOrder {
+        public static bool IsPopOrder(int[] push, int[] pop) {
+            if (push == null || pop == null) {
+                throw new Exception("Invalid argument.");
+            }
+
+            if (push.Length <= 0 || pop.Length <= 0 || push.Length != pop.Length) {
+                throw new Exception("Invalid argument.");
+            }
+
+            bool possible = false;
+            var stackData = new Stack<int>();
+            int nextPush = 0;
+            int nextPop = 0;
+
+            while (nextPop < pop.Length) {
+                while (stackData.Count == 0 || stackData.Peek() != pop[nextPop]) {
+                    if (nextPush >= push.Length) {
+                        break;
+                    }
+
+                    stackData.Push(push[nextPush]);
+                    nextPush++;
+                }
+
+                if (stackData.Peek() != pop[nextPop]) {
+                    break;
+                }
+
+                stackData.Pop();
+                nextPop++;
+            }
+
+            if (stackData.Count == 0 && nextPop >= pop.Length) {
+                possible = true;
+            }
+
+            return possible;
+        }
+    }
+}
---
+++ src/case-31/StackPushPopOrderTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace CodingInterview {
+    [TestFixture]
+    public class StackPushPopOrderTest {
+        [Test]
+        public void TestIsPopOrder() {
+            int[] push = {1, 2, 3, 4, 5};
+            int[] rightPop = {4, 5, 3, 2, 1};
+            Assert.AreEqual(true, StackPushPopOrder.IsPopOrder(push, rightPop));
+
+            int[] wrongPop = {4, 3, 5, 1, 2};
+            Assert.AreEqual(false, StackPushPopOrder.IsPopOrder(push, wrongPop));
+        }
+    }
+}
