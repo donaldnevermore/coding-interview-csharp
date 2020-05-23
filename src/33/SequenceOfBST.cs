dd1193fc0188455c2d3b0439df2c2d8206086dfb
Dongcheng Wang <dongcheng6@qq.com>
2020-05-23 21:58:11 +0800

    Add SequenceOfBST

---
+++ src/33/SequenceOfBST.cs
@@ -0,0 +1,41 @@
+﻿namespace CodingInterview {
+    public class SequenceOfBST {
+        public static bool IsSequenceOfBST(int[] sequence) {
+            if (sequence == null || sequence.Length <= 0) {
+                return false;
+            }
+
+            int root = sequence[^1];
+
+            int i = 0;
+            while (i < sequence.Length - 1) {
+                if (sequence[i] > root) {
+                    break;
+                }
+
+                i++;
+            }
+
+            int j = i;
+            while (j < sequence.Length - 1) {
+                if (sequence[j] < root) {
+                    return false;
+                }
+
+                j++;
+            }
+
+            bool left = true;
+            if (i > 0) {
+                left = IsSequenceOfBST(sequence[..i]);
+            }
+
+            bool right = true;
+            if (j < sequence.Length - 1) {
+                right = IsSequenceOfBST(sequence[i..^1]);
+            }
+
+            return left && right;
+        }
+    }
+}
---
+++ src/33/SequenceOfBSTTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace CodingInterview {
+    [TestFixture]
+    public class SequenceOfBSTTest {
+        [Test]
+        public void TestIsSequenceOfBST() {
+            Assert.AreEqual(true, SequenceOfBST.IsSequenceOfBST(new int[] {
+                5, 7, 6, 9, 11, 10, 8
+            }));
+            Assert.AreEqual(false, SequenceOfBST.IsSequenceOfBST(new int[] {
+                7, 4, 6, 5
+            }));
+        }
+    }
+}
