b15090bfedc6e3b046efe5ffbf99d1c28305ded1
Dongcheng Wang <dongcheng6@qq.com>
2020-08-21 16:01:07 +0800

    Add question 37

---
+++ src/37/SerializeBinaryTree.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CodingInterview
+{
+    public class SerializeBinaryTree
+    {
+        public static void Serialize(BinaryTreeNode root)
+        {
+            if (root == null)
+            {
+                Console.Write("$,");
+                return;
+            }
+
+            Console.Write($"{root.Value},");
+            Serialize(root.Left);
+            Serialize(root.Right);
+        }
+
+        public static void Deserialize(ref BinaryTreeNode root)
+        {
+            int number;
+
+            number = 0;
+            if (true)
+            {
+                root = new BinaryTreeNode();
+                root.Value = number;
+                root.Left = null;
+                root.Right = null;
+
+                var left = root.Left;
+                Deserialize(ref left);
+                var right = root.Right;
+                Deserialize(ref right);
+            }
+        }
+    }
+}
