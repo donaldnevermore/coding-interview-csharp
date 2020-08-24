3db280ac6ce93bd329747044aff74902127fc80e
Dongcheng Wang <dongcheng6@qq.com>
2020-08-24 11:44:17 +0800

    Add MoreThanHalfNumber

---
+++ src/39/MoreThanHalfNumber.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace CodingInterview
+{
+    public class MoreThanHalfNumber
+    {
+        public static int Get(int[] numbers)
+        {
+            if (numbers == null || numbers.Length <= 0)
+            {
+                throw new ArgumentException("Invalid input.");
+            }
+
+            var result = numbers[0];
+            var times = 1;
+
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                if (times == 0)
+                {
+                    result = numbers[i];
+                    times = 1;
+                }
+                else if (numbers[i] == result)
+                {
+                    times++;
+                }
+                else
+                {
+                    times--;
+                }
+            }
+
+            if (!CheckMoreThanHalf(numbers, result))
+            {
+                throw new ArgumentException("Invalid input.");
+            }
+
+            return result;
+        }
+
+        private static bool CheckMoreThanHalf(int[] numbers, int result)
+        {
+            var times = 0;
+            foreach (var number in numbers)
+            {
+                if (number == result)
+                {
+                    times++;
+                }
+            }
+
+            if (times * 2 <= numbers.Length)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ src/39/MoreThanHalfNumberTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace CodingInterview
+{
+    [TestFixture]
+    public class MoreThanHalfNumberTest
+    {
+        [Test]
+        public void TestMoreThanHalfNumber()
+        {
+            var result = MoreThanHalfNumber.Get(new[] {1, 2, 3, 2, 2, 2, 5, 4, 2});
+            Assert.AreEqual(2, result);
+        }
+    }
+}
