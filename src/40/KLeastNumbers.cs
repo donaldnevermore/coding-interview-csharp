155f9ac1d76e196398df1aaca2c78b5fee39c685
Dongcheng Wang <dongcheng6@qq.com>
2020-08-25 10:17:39 +0800

    Add KLeastNumbers

---
+++ src/40/KLeastNumbers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodingInterview
+{
+    public class KLeastNumbers
+    {
+        public static int[] Get(int[] data, int k)
+        {
+            if (k < 1 || data.Length < k)
+            {
+                throw new ArgumentException("Invalid input.");
+            }
+
+            var leastNumbers = new SortedSet<int>();
+            foreach (var number in data)
+            {
+                if (leastNumbers.Count < k)
+                {
+                    leastNumbers.Add(number);
+                }
+                else
+                {
+                    var greatest = leastNumbers.Max;
+                    if (number < greatest)
+                    {
+                        leastNumbers.Remove(greatest);
+                        leastNumbers.Add(number);
+                    }
+                }
+            }
+
+            return leastNumbers.ToArray();
+        }
+    }
+}
---
+++ src/40/KLeastNumbersTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace CodingInterview
+{
+    [TestFixture]
+    public class KLeastNumbersTest
+    {
+        [Test]
+        public void Test()
+        {
+            var arr = new[] {4, 5, 1, 6, 2, 7, 3, 8};
+            var expected = new[] {1, 2, 3, 4};
+            var actual = KLeastNumbers.Get(arr, 4);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
