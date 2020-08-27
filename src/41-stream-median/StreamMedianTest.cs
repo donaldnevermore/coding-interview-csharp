46f4c25381b2f85c6cd6f713e018a77e92bb6e05
Dongcheng Wang <dongcheng6@qq.com>
2020-08-27 20:37:44 +0800

    Add StreamMedian

---
+++ src/41-stream-median/StreamMedianTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace CodingInterview
+{
+    [TestFixture]
+    public class StreamMedianTest
+    {
+        [Test]
+        public void Test()
+        {
+            var sm = new StreamMedian();
+            sm.Insert(2);
+            sm.Insert(4);
+            sm.Insert(6);
+
+            var r1 = sm.GetMedian();
+            Assert.AreEqual(4, r1);
+
+            sm.Insert(8);
+            var r2 = sm.GetMedian();
+            Assert.AreEqual(5, r2);
+        }
+    }
+}
