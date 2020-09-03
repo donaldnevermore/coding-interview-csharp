ec2a84d2550eedffbd1924ec4f46bf121bfc58b4
Dongcheng Wang <dongcheng6@qq.com>
2020-09-03 16:49:33 +0800

    Add GreatestSumOfSubArrays

---
+++ src/42-greatest-sum-of-sub-arrays/GreatestSumOfSubArrays.cs
@@ -0,0 +1,35 @@
+﻿namespace CodingInterview
+{
+    public class GreatestSumOfSubArrays
+    {
+        public static int Find(int[] data)
+        {
+            if (data == null || data.Length <= 0)
+            {
+                return 0;
+            }
+
+            var currentSum = 0;
+            var greatestSum = 0;
+
+            foreach (var num in data)
+            {
+                if (currentSum <= 0)
+                {
+                    currentSum = num;
+                }
+                else
+                {
+                    currentSum += num;
+                }
+
+                if (currentSum > greatestSum)
+                {
+                    greatestSum = currentSum;
+                }
+            }
+
+            return greatestSum;
+        }
+    }
+}
---
+++ src/42-greatest-sum-of-sub-arrays/GreatestSumOfSubArraysTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace CodingInterview
+{
+    [TestFixture]
+    public class GreatestSumOfSubArraysTest
+    {
+        [Test]
+        public void TestFind()
+        {
+            var arr = new[] {1, -2, 3, 10, -4, 7, 2, -5};
+            var result = GreatestSumOfSubArrays.Find(arr);
+            Assert.AreEqual(18, result);
+        }
+    }
+}
