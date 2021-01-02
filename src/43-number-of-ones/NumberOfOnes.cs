4d1ffaae52b7ef1f3cadea14bce16d22dfa2f604
Donald Wang <dongcheng6@qq.com>
2021-01-02 22:46:22 +0800

    Update to .net5.0

---
+++ src/43-number-of-ones/NumberOfOnes.cs
@@ -0,0 +1,20 @@
+﻿namespace CodingInterview
+{
+    public class NumberOfOnes
+    {
+        public static int NumberOfOnesBetween(int n)
+        {
+            if (n <= 0)
+            {
+                return 0;
+            }
+
+            return NumberOf(n.ToString());
+        }
+
+        private static int NumberOf(string n)
+        {
+            return 0;
+        }
+    }
+}
