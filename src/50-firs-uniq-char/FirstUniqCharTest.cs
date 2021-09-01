31e5aee12fb6b9c9a2a632486d4b9e1dc8023982
Donald D. Nevermore <donald33wang@gmail.com>
2021-09-01 17:27:07 +0800

    Add question 50

---
+++ src/50-firs-uniq-char/FirstUniqCharTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace CodingInterview {
+    [TestFixture]
+    public class FirstUniqCharTest {
+        [Test]
+        public void Test() {
+            var s = "abaccdeff";
+            var c = FirstUniqChar.Get(s);
+            Assert.AreEqual('b', c);
+
+            var s2 = "";
+            var c2 = FirstUniqChar.Get(s2);
+            Assert.AreEqual(' ', c2);
+        }
+    }
+}
