9adf06bd8f520a60f2dbf7bd3735dc8ad9adc315
Donald D. Nevermore <donald33wang@gmail.com>
2021-09-01 16:59:05 +0800

    Update questions 02 & 03

--- src/02-singleton/Singleton.cs
+++ src/02-singleton/Singleton.cs
@@ -1,19 +1,19 @@
 ﻿namespace CodingInterview {
     public sealed class Singleton {
-        public static readonly Singleton Instance = new Singleton();
+        public static Singleton Instance => new Singleton();
 
         private Singleton() {
         }
     }
 
     public sealed class Singleton5 {
-        public static readonly Singleton5 Instance = Nested.instance;
+        public static Singleton5 Instance => Nested.Instance;
 
         private Singleton5() {
         }
 
         private class Nested {
-            internal static readonly Singleton5 instance = new Singleton5();
+            internal static Singleton5 Instance => new Singleton5();
 
             static Nested() {
             }
---
+++ src/03-find-repeat-number/FindRepeatNumberTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace CodingInterview {
+    [TestFixture]
+    public class FindRepeatNumberTest {
+        [Test]
+        public static void TestGet() {
+            var a = new[] { 2, 3, 1, 0, 2, 5, 3 };
+            var r = FindRepeatNumber.Get(a);
+            Assert.IsTrue(r == 2 || r == 3);
+
+            var a2 = new[] { 2, 3, 5, 4, 3, 2, 6, 7 };
+            var r2 = FindRepeatNumber.GetWithArrayIntact(a2);
+            Assert.IsTrue(r2 == 2 || r2 == 3);
+        }
+    }
+}
