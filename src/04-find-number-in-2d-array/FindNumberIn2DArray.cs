7ee9fe525d8293dcb07425ea673fc480ad8fe70e
Donald D. Nevermore <donald33wang@gmail.com>
2021-09-02 21:31:32 +0800

    Update question 04

---
+++ src/04-find-number-in-2d-array/FindNumberIn2DArray.cs
@@ -0,0 +1,30 @@
+﻿namespace CodingInterview {
+    public class FindNumberIn2DArray {
+        public static bool Find(int[][] matrix, int target) {
+            if (matrix.Length <= 0 || matrix[0].Length <= 0) {
+                return false;
+            }
+
+            var rows = matrix.Length;
+            var columns = matrix[0].Length;
+
+            var row = 0;
+            var column = columns - 1;
+
+            while (row < rows && column >= 0) {
+                if (matrix[row][column] == target) {
+                    return true;
+                }
+
+                if (matrix[row][column] > target) {
+                    column--;
+                }
+                else {
+                    row++;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ src/04-find-number-in-2d-array/FindNumberIn2DArrayTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace CodingInterview {
+    [TestFixture]
+    public class FindNumberIn2DArrayTest {
+        [Test]
+        public void Test() {
+            var m = new[] {
+                new[] { 1, 4, 7, 11, 15 },
+                new[] { 2, 5, 8, 12, 19 },
+                new[] { 3, 6, 9, 16, 22 },
+                new[] { 10, 13, 14, 17, 24 },
+                new[] { 18, 21, 23, 26, 30 }
+            };
+            var b = FindNumberIn2DArray.Find(m, 5);
+            var b2 = FindNumberIn2DArray.Find(m, 20);
+            Assert.IsTrue(b);
+            Assert.IsFalse(b2);
+        }
+    }
+}
