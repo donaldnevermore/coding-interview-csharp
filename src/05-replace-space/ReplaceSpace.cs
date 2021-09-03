71bcdd685631a4f55c4e2082e1b434836ad5822e
Donald D. Nevermore <donald33wang@gmail.com>
2021-09-03 15:42:43 +0800

    Update question 05

---
+++ src/05-replace-space/ReplaceSpace.cs
@@ -0,0 +1,44 @@
+﻿namespace CodingInterview {
+    public class ReplaceSpace {
+        /// <summary>
+        /// Replace every blank with "%20" in the string.
+        /// </summary>
+        public static string Replace(string s) {
+            if (s.Length <= 0) {
+                return "";
+            }
+
+            // Calculate the new length of the string.
+            var blankCount = 0;
+            for (var i = 0; i < s.Length; i++) {
+                if (s[i] == ' ') {
+                    blankCount++;
+                }
+            }
+
+            var newLength = s.Length + blankCount * 2;
+            var newArr = new char[newLength];
+            var oldIndex = s.Length - 1;
+            var newIndex = newArr.Length - 1;
+            // Replace blanks from right to left.
+            while (oldIndex >= 0 && newIndex >= oldIndex) {
+                if (s[oldIndex] == ' ') {
+                    newArr[newIndex] = '0';
+                    newIndex--;
+                    newArr[newIndex] = '2';
+                    newIndex--;
+                    newArr[newIndex] = '%';
+                    newIndex--;
+                }
+                else {
+                    newArr[newIndex] = s[oldIndex];
+                    newIndex--;
+                }
+
+                oldIndex--;
+            }
+
+            return string.Concat(newArr);
+        }
+    }
+}
---
+++ src/05-replace-space/ReplaceSpaceTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace CodingInterview.Test {
+    [TestFixture]
+    public class ReplaceSpaceTest {
+        [Test]
+        public void TestReplace() {
+            var str = "We are happy.";
+            var newStr = "We%20are%20happy.";
+            var r = ReplaceSpace.Replace(str);
+            Assert.AreEqual(newStr, r);
+        }
+    }
+}
