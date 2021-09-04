c77f0e508165881680f6cc6442a1c7478239bcc4
Donald D. Nevermore <donald33wang@gmail.com>
2021-09-04 11:36:00 +0800

    Add question 52

---
+++ src/52-get-intersection-node/GetIntersectionNode.cs
@@ -0,0 +1,43 @@
+﻿namespace CodingInterview {
+    public class GetIntersectionNode {
+        public ListNode? Get(ListNode? headA, ListNode? headB) {
+            if (headA is null || headB is null) {
+                return null;
+            }
+
+            var headALength = CountLength(headA);
+            var headBLength = CountLength(headB);
+            var lengthDiff = headALength - headBLength;
+
+            var longHead = headA;
+            var shortHead = headB;
+            if (headBLength > headALength) {
+                longHead = headB;
+                shortHead = headA;
+                lengthDiff = headBLength - headALength;
+            }
+
+            for (var i = 0; i < lengthDiff; i++) {
+                longHead = longHead?.Next;
+            }
+
+            while (longHead is not null && shortHead is not null && longHead != shortHead) {
+                longHead = longHead.Next;
+                shortHead = shortHead.Next;
+            }
+
+            return longHead;
+        }
+
+        private int CountLength(ListNode? head) {
+            var count = 0;
+            var node = head;
+            while (node is not null) {
+                count++;
+                node = node.Next;
+            }
+
+            return count;
+        }
+    }
+}
