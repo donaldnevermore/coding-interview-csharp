24541195648d2ead21f82b73d44455d6b7e78de1
Donald D. Nevermore <donald33wang@gmail.com>
2021-09-04 11:09:38 +0800

    Update question 06

---
+++ src/06-reverse-print/ReversePrint.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CodingInterview {
+    public class ReversePrint {
+        public int[] Reverse(ListNode? head) {
+            if (head is null) {
+                return Array.Empty<int>();
+            }
+
+            var len = 0;
+            var node = head;
+            while (node is not null) {
+                len++;
+                node = node.Next;
+            }
+
+            var arr = new int[len];
+            var i = len - 1;
+            node = head;
+            while (node is not null && i >= 0) {
+                arr[i] = node.Val;
+                node = node.Next;
+                i--;
+            }
+
+            return arr;
+        }
+    }
+}
