fd9239680d74443a3f9beae29baf6ad8a8047e5a
Donald D. Nevermore <donald33wang@gmail.com>
2021-09-06 22:46:55 +0800

    Add question 53

---
+++ src/53-search/Search.cs
@@ -0,0 +1,66 @@
+﻿namespace CodingInterview {
+    public class Search {
+        public static int Get(int[] nums, int target) {
+            if (nums.Length <= 0) {
+                return 0;
+            }
+
+            var firstK = GetFirstK(nums, target, 0, nums.Length - 1);
+            var lastK = GetLastK(nums, target, 0, nums.Length - 1);
+
+            if (firstK >= 0 && lastK >= 0) {
+                return lastK - firstK + 1;
+            }
+
+            return 0;
+        }
+
+        private static int GetFirstK(int[] nums, int target, int start, int end) {
+            if (start > end) {
+                return -1;
+            }
+
+            var mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                if ((mid > 0 && nums[mid - 1] != target) || mid == 0) {
+                    return mid;
+                }
+                else {
+                    end = mid - 1;
+                }
+            }
+            else if (nums[mid] > target) {
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
+            }
+
+            return GetFirstK(nums, target, start, end);
+        }
+
+        private static int GetLastK(int[] nums, int target, int start, int end) {
+            if (start > end) {
+                return -1;
+            }
+
+            var mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                if ((mid < nums.Length - 1 && nums[mid + 1] != target) || mid == nums.Length - 1) {
+                    return mid;
+                }
+                else {
+                    start = mid + 1;
+                }
+            }
+            else if (nums[mid] > target) {
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
+            }
+
+            return GetLastK(nums, target, start, end);
+        }
+    }
+}
---
+++ src/53-search/SearchTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace CodingInterview {
+    [TestFixture]
+    public class SearchTest {
+        [Test]
+        public void Test() {
+            var nums = new[] { 5, 7, 7, 8, 8, 10 };
+            var got = Search.Get(nums, 8);
+            Assert.AreEqual(2, got);
+
+            var nums2 = new[] { 5, 7, 7, 8, 8, 10 };
+            var got2 = Search.Get(nums2, 6);
+            Assert.AreEqual(0, got2);
+        }
+    }
+}
