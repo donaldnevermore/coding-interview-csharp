b291534237c451c660f636061e1ff6cf4badb177
Donald D. Nevermore <donald33wang@gmail.com>
2021-09-08 21:48:36 +0800

    Add question 54

---
+++ src/54-kth-largest/KthLargest.cs
@@ -0,0 +1,38 @@
+﻿namespace CodingInterview {
+    public class KthLargest {
+        private int k;
+        private TreeNode? target = null;
+
+        public int Get(TreeNode? root, int k) {
+            this.k = k;
+
+            DFS(root);
+
+            if (target is null) {
+                return -1;
+            }
+
+            return target.Val;
+        }
+
+        private void DFS(TreeNode? root) {
+            if (root is null) {
+                return;
+            }
+
+            DFS(root.Right);
+
+            if (k == 0) {
+                return;
+            }
+
+            if (k == 1) {
+                target = root;
+            }
+
+            k--;
+
+            DFS(root.Left);
+        }
+    }
+}
---
+++ src/54-kth-largest/KthLargestTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace CodingInterview {
+    [TestFixture]
+    public class KthLargestTest {
+        [Test]
+        public void Test() {
+            var node2 = new TreeNode { Val = 2 };
+            var node1 = new TreeNode { Val = 1, Right = node2 };
+            var node4 = new TreeNode { Val = 4 };
+            var root = new TreeNode { Val = 3, Left = node1, Right = node4 };
+
+            var obj = new KthLargest();
+            var got = obj.Get(root, 1);
+            Assert.AreEqual(4, got);
+        }
+    }
+}
