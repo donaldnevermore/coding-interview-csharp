736a2ff4f8b7fb4f36bbd86cf18af4194c2641a3
Donald D. Nevermore <donald33wang@gmail.com>
2021-09-11 22:16:27 +0800

    Add question 56

---
+++ src/56-single-numbers/SingleNumbers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CodingInterview {
+    public class SingleNumbers {
+        public static int[] Find(int[] nums) {
+            if (nums.Length < 2) {
+                return Array.Empty<int>();
+            }
+
+            var xorResult = GetXorResult(nums);
+            var lowBit1 = GetLowBit1(xorResult);
+            var arr = new int[2];
+
+            for (var i = 0; i < nums.Length; i++) {
+                if (IsBit1(nums[i], lowBit1)) {
+                    arr[0] ^= nums[i];
+                }
+                else {
+                    arr[1] ^= nums[i];
+                }
+            }
+
+            return arr;
+        }
+
+        private static int GetXorResult(int[] nums) {
+            var n = 0;
+            for (var i = 0; i < nums.Length; i++) {
+                n ^= nums[i];
+            }
+
+            return n;
+        }
+
+        private static int GetLowBit1(int num) {
+            return num & -num;
+        }
+
+        private static bool IsBit1(int num, int lowBit) {
+            return (num & lowBit) > 0;
+        }
+    }
+}
---
+++ src/56-single-numbers/SingleNumbersTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace CodingInterview {
+    [TestFixture]
+    public class SingleNumbersTest {
+        [Test]
+        public void Test() {
+            var arr = new[] { 4, 1, 4, 6 };
+            var res1 = new[] { 1, 6 };
+            var res2 = new[] { 6, 1 };
+            var got = SingleNumbers.Find(arr);
+            var t = got.SequenceEqual(res1) || got.SequenceEqual(res2);
+            Assert.IsTrue(t);
+        }
+
+        [Test]
+        public void Test2() {
+            var arr = new[] { 1, 2, 10, 4, 1, 4, 3, 3 };
+            var res1 = new[] { 2, 10 };
+            var res2 = new[] { 10, 2 };
+            var got = SingleNumbers.Find(arr);
+            var t = got.SequenceEqual(res1) || got.SequenceEqual(res2);
+            Assert.IsTrue(t);
+        }
+    }
+}
