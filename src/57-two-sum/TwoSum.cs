65e1888cc30f6c160ae44d3362442e016d086820
Donald D. Nevermore <donald33wang@gmail.com>
2021-09-13 22:02:26 +0800

    Add question 57-2

--- src/57-two-sum/TwoSum.cs
+++ src/57-two-sum/TwoSum.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CodingInterview {
     public class TwoSum {
@@ -24,6 +25,53 @@ namespace CodingInterview {
             }
 
             return Array.Empty<int>();
+        }
+
+        public static int[][] FindContinuousSequence(int target) {
+            if (target < 1) {
+                return Array.Empty<int[]>();
+            }
+
+            var list = new List<int[]>();
+            var small = 1;
+            var big = 2;
+            var mid = (target + 1) / 2;
+            var sum = small + big;
+
+            while (small < mid) {
+                if (sum == target) {
+                    AddContinuousSequence(list, small, big);
+                }
+
+                while (sum > target && small < mid) {
+                    sum -= small;
+                    small++;
+
+                    if (sum == target) {
+                        AddContinuousSequence(list, small, big);
+                    }
+                }
+
+                big++;
+                sum += big;
+            }
+
+            return list.ToArray();
+
+            static void AddContinuousSequence(List<int[]> list, int small, int big) {
+                var len = big - small + 1;
+                var a = new int[len];
+
+                var i = 0;
+                var j = small;
+                while (i < len && j <= big) {
+                    a[i] = j;
+                    j++;
+                    i++;
+                }
+
+                list.Add(a);
+            }
         }
     }
 }
