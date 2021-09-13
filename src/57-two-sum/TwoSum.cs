fab816007c7465fd8ab202268403b61e2b1bf82c
Donald D. Nevermore <donald33wang@gmail.com>
2021-09-13 19:32:25 +0800

    Add question 57

---
+++ src/57-two-sum/TwoSum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CodingInterview {
+    public class TwoSum {
+        public static int[] Get(int[] nums, int target) {
+            if (nums.Length < 1) {
+                return Array.Empty<int>();
+            }
+
+            var left = 0;
+            var right = nums.Length - 1;
+
+            while (left < right) {
+                var sum = nums[left] + nums[right];
+                if (sum == target) {
+                    return new[] { nums[left], nums[right] };
+                }
+                else if (sum > target) {
+                    right--;
+                }
+                else {
+                    left++;
+                }
+            }
+
+            return Array.Empty<int>();
+        }
+    }
+}
