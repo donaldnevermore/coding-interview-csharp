554395a55d3cbda05f6fd117702ad6e6f10e5b22
Donald D. Nevermore <donald33wang@gmail.com>
2021-09-16 23:12:46 +0800

    Update question 10-2

--- src/10-fib/Fib.cs
+++ src/10-fib/Fib.cs
@@ -1,5 +1,23 @@
 ﻿namespace CodingInterview {
     public class Fib {
+        public static int Iterative(int n) {
+            if (n <= 1) {
+                return n;
+            }
+
+            const int Mod = 1000000007;
+            var nMinus1 = 1;
+            var nMinus2 = 0;
+            var total = 0;
+            for (var i = 2; i <= n; i++) {
+                total = (nMinus1 + nMinus2) % Mod;
+                nMinus2 = nMinus1;
+                nMinus1 = total;
+            }
+
+            return total;
+        }
+
         public static int Recursive(int n) {
             if (n <= 1) {
                 return n;
@@ -8,14 +26,14 @@
             return Recursive(n - 1) + Recursive(n - 2);
         }
 
-        public static int Loop(int n) {
+        public static int NumWays(int n) {
             if (n <= 1) {
-                return n;
+                return 1;
             }
 
             const int Mod = 1000000007;
             var nMinus1 = 1;
-            var nMinus2 = 0;
+            var nMinus2 = 1;
             var total = 0;
             for (var i = 2; i <= n; i++) {
                 total = (nMinus1 + nMinus2) % Mod;
