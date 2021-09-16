c687736e4761413cc860e26916a6d9b1fa52f5ec
Donald D. Nevermore <donald33wang@gmail.com>
2021-09-16 23:00:39 +0800

    Add question 58

---
+++ src/58-reverse-words/ReverseWords.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+
+namespace CodingInterview {
+    public class ReverseWords {
+        public static string Reverse(string s) {
+            if (s.Length == 0) {
+                return string.Empty;
+            }
+
+            s = s.Trim();
+
+            var sb = new StringBuilder();
+            var i = s.Length - 1;
+            var j = i;
+
+            while (i >= 0) {
+                while (i >= 0 && s[i] != ' ') {
+                    i--;
+                }
+
+                sb.Append(s[(i + 1)..(j + 1)] + " ");
+
+                while (i >= 0 && s[i] == ' ') {
+                    i--;
+                }
+
+                j = i;
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        public static string ReverseLeftWords(string s, int n) {
+            if (s.Length == 0) {
+                return string.Empty;
+            }
+            if (n <= 0) {
+                return s;
+            }
+
+            var sb = new StringBuilder();
+
+            for (var i = n; i < s.Length; i++) {
+                sb.Append(s[i]);
+            }
+            for (var j = 0; j < n; j++) {
+                sb.Append(s[j]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ src/58-reverse-words/ReverseWordsTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace CodingInterview {
+    [TestFixture]
+    public class ReverseWordsTest {
+        [Test]
+        public void Test() {
+            var s = "I am a student.";
+            var want = "student. a am I";
+            var got = ReverseWords.Reverse(s);
+            Assert.AreEqual(want, got);
+        }
+
+        [Test]
+        public void Test2() {
+            var s = "lrloseumgh";
+            var want = "umghlrlose";
+            var got = ReverseWords.ReverseLeftWords(s, 6);
+            Assert.AreEqual(want, got);
+        }
+    }
+}
