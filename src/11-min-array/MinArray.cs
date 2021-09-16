091217c0bf3fb9340b3aafe1db7d6677b2fa8cdb
Donald D. Nevermore <donald33wang@gmail.com>
2021-09-16 23:35:00 +0800

    Update question 11

---
+++ src/11-min-array/MinArray.cs
@@ -0,0 +1,48 @@
+﻿namespace CodingInterview {
+    public class MinArray {
+        public static int MinNumber(int[] numbers) {
+            if (numbers.Length == 0) {
+                return -1;
+            }
+
+            var left = 0;
+            var right = numbers.Length - 1;
+            // Rotated array could be the same as the original.
+            var mid = left;
+
+            while (numbers[left] >= numbers[right]) {
+                if (right - left == 1) {
+                    mid = right;
+                    break;
+                }
+
+                mid = (left + right) / 2;
+
+                // If all three numbers are equal, we can only do a linear search.
+                if (numbers[left] == numbers[mid] && numbers[mid] == numbers[right]) {
+                    return MinInOrder(numbers, left, right);
+                }
+
+                if (numbers[mid] >= numbers[left]) {
+                    left = mid;
+                }
+                else if (numbers[mid] <= numbers[right]) {
+                    right = mid;
+                }
+            }
+
+            return numbers[mid];
+        }
+
+        private static int MinInOrder(int[] numbers, int left, int right) {
+            var result = numbers[left];
+            for (int i = left + 1; i <= right; i++) {
+                if (numbers[i] < result) {
+                    result = numbers[i];
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/11-min-array/MinArrayTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace CodingInterview {
+    [TestFixture]
+    public class MinArrayTest {
+        [Test]
+        public void TestMin() {
+            var arr = new[] { 3, 4, 5, 1, 2 };
+            var got = MinArray.MinNumber(arr);
+            Assert.AreEqual(1, got);
+
+            var arr2 = new[] { 2, 2, 2, 0, 1 };
+            var got2 = MinArray.MinNumber(arr2);
+            Assert.AreEqual(0, got2);
+        }
+    }
+}
