54ede95e197d0df780b6ffe26489977584728be0
Donald D. Nevermore <donald33wang@gmail.com>
2021-09-25 21:20:56 +0800

    Update question 12

---
+++ src/12-exist/Exist.cs
@@ -0,0 +1,56 @@
+﻿namespace CodingInterview {
+    public class Exist {
+        public static bool HasPath(char[][] board, string word) {
+            if (board.Length == 0 || string.IsNullOrEmpty(word)) {
+                return false;
+            }
+
+            var rows = board.Length;
+            var cols = board[0].Length;
+
+            var visited = new bool[rows, cols];
+
+            var pathLength = 0;
+            for (int row = 0; row < rows; row++) {
+                for (int col = 0; col < cols; col++) {
+                    if (HasPathCore(row, col)) {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+
+            bool HasPathCore(int row, int col) {
+                // Reach the end
+                if (pathLength > (word.Length - 1)) {
+                    return true;
+                }
+
+                var hasPath = false;
+
+                if (row >= 0 &&
+                    row < rows &&
+                    col >= 0 &&
+                    col < cols &&
+                    board[row][col] == word[pathLength] &&
+                    !visited[row, col]) {
+                    pathLength++;
+                    visited[row, col] = true;
+
+                    hasPath = HasPathCore(row, col - 1) ||
+                              HasPathCore(row - 1, col) ||
+                              HasPathCore(row, col + 1) ||
+                              HasPathCore(row + 1, col);
+
+                    if (!hasPath) {
+                        pathLength--;
+                        visited[row, col] = false;
+                    }
+                }
+
+                return hasPath;
+            }
+        }
+    }
+}
---
+++ src/12-exist/ExistTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace CodingInterview {
+    [TestFixture]
+    public class ExistTest {
+        [Test]
+        public void TestHasPath() {
+            var board = new[] {
+                new[] { 'A', 'B', 'C', 'E' },
+                new[] { 'S', 'F', 'C', 'S' },
+                new[] { 'A', 'D', 'E', 'E' }
+            };
+
+            var got = Exist.HasPath(board, "ABCCED");
+            Assert.AreEqual(true, got);
+        }
+    }
+}
