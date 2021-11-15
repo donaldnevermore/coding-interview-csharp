290199822c814f437641c8198e74f82d8ee587db
Donald D. Nevermore <donald33wang@gmail.com>
2021-11-15 16:10:00 +0800

    Update 14-2

--- src/14-cutting-rope/CuttingRope.cs
+++ src/14-cutting-rope/CuttingRope.cs
@@ -1,70 +1,67 @@
-﻿using System;
+namespace CodingInterview;
 
-namespace CodingInterview {
-    public class CuttingRope {
-        public static int MaxProductDynamic(int n) {
-            if (n < 2) {
-                return 0;
-            }
+public class CuttingRope {
+    public static int MaxProductDP(int n) {
+        if (n < 2) {
+            return 0;
+        }
 
-            if (n == 2) {
-                return 1;
-            }
+        if (n == 2) {
+            return 1;
+        }
 
-            if (n == 3) {
-                return 2;
-            }
+        if (n == 3) {
+            return 2;
+        }
 
-            var products = new int[n + 1];
+        var products = new int[n + 1];
 
-            // Because lengths less than or equal to 3 are immediately returned,
-            // we set these initial values only for calculation.
-            products[0] = 0;
-            products[1] = 1;
-            products[2] = 2;
-            products[3] = 3;
+        // Because lengths less than or equal to 3 are immediately returned,
+        // we set these initial values only for calculation.
+        products[0] = 0;
+        products[1] = 1;
+        products[2] = 2;
+        products[3] = 3;
 
-            int max;
-            for (var i = 4; i <= n; i++) {
-                max = 0;
-                for (var j = 1; j <= i / 2; j++) {
-                    var product = products[j] * products[i - j];
-                    if (max < product) {
-                        max = product;
-                    }
-
-                    products[i] = max;
+        int max;
+        for (var i = 4; i <= n; i++) {
+            max = 0;
+            for (var j = 1; j <= i / 2; j++) {
+                var product = products[j] * products[i - j];
+                if (max < product) {
+                    max = product;
                 }
-            }
 
-            max = products[n];
-            return max;
+                products[i] = max;
+            }
         }
 
-        public static int MaxProductGreedy(int n) {
-            if (n < 2) {
-                return 0;
-            }
+        max = products[n];
+        return max;
+    }
 
-            if (n == 2) {
-                return 1;
-            }
-
-            if (n == 3) {
-                return 2;
-            }
-
-            // Cut the rope by 3 as more as possible.
-            var timesOfThrees = n / 3;
-
-            // When there's only 4 left,
-            // cut it by 2*2 rather than 3*1
-            if (n - timesOfThrees * 3 == 1) {
-                timesOfThrees--;
-            }
-
-            var timesOfTwos = (n - timesOfThrees * 3) / 2;
-            return (int)(Math.Pow(3, timesOfThrees) * Math.Pow(2, timesOfTwos));
+    public static int MaxProductGreedy(int n) {
+        if (n <= 0) {
+            return 0;
         }
+        if (n <= 3) {
+            return n - 1;
+        }
+        if (n == 4) {
+            return 4;
+        }
+
+        var product = 1L;
+        const int Mod = 1000000007;
+
+        // Cut the rope by 3 as more as possible.
+        // When there's only 4 left, cut it by 2*2 rather than 3*1.
+        while (n > 4) {
+            product = product * 3 % Mod;
+            n -= 3;
+        }
+
+        // Plus what remains. It can only be 2, 3, or 4.
+        return (int)(product * n % Mod);
     }
 }
--- src/14-cutting-rope/CuttingRopeTest.cs
+++ src/14-cutting-rope/CuttingRopeTest.cs
@@ -1,24 +1,24 @@
-﻿using NUnit.Framework;
+namespace CodingInterview;
 
-namespace CodingInterview.Test {
-    [TestFixture]
-    public class CuttingRopeTest {
-        [Test]
-        public void TestMaxProductDynamic() {
-            Assert.AreEqual(1, CuttingRope.MaxProductDynamic(2));
-            Assert.AreEqual(4, CuttingRope.MaxProductDynamic(4));
-            Assert.AreEqual(18, CuttingRope.MaxProductDynamic(8));
-            Assert.AreEqual(27, CuttingRope.MaxProductDynamic(9));
-            Assert.AreEqual(36, CuttingRope.MaxProductDynamic(10));
-        }
+using NUnit.Framework;
 
-        [Test]
-        public void TestMaxProductGreedy() {
-            Assert.AreEqual(1, CuttingRope.MaxProductGreedy(2));
-            Assert.AreEqual(4, CuttingRope.MaxProductGreedy(4));
-            Assert.AreEqual(18, CuttingRope.MaxProductGreedy(8));
-            Assert.AreEqual(27, CuttingRope.MaxProductGreedy(9));
-            Assert.AreEqual(36, CuttingRope.MaxProductGreedy(10));
-        }
+[TestFixture]
+public class CuttingRopeTest {
+    [Test]
+    public void TestMaxProductDP() {
+        Assert.AreEqual(1, CuttingRope.MaxProductDP(2));
+        Assert.AreEqual(4, CuttingRope.MaxProductDP(4));
+        Assert.AreEqual(18, CuttingRope.MaxProductDP(8));
+        Assert.AreEqual(27, CuttingRope.MaxProductDP(9));
+        Assert.AreEqual(36, CuttingRope.MaxProductDP(10));
+    }
+
+    [Test]
+    public void TestMaxProductGreedy() {
+        Assert.AreEqual(1, CuttingRope.MaxProductGreedy(2));
+        Assert.AreEqual(4, CuttingRope.MaxProductGreedy(4));
+        Assert.AreEqual(18, CuttingRope.MaxProductGreedy(8));
+        Assert.AreEqual(27, CuttingRope.MaxProductGreedy(9));
+        Assert.AreEqual(36, CuttingRope.MaxProductGreedy(10));
     }
 }
