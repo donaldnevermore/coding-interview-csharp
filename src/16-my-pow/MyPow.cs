bb96e1770238c3a4626060a77bcdb781b284069c
Donald D. Nevermore <donald33wang@gmail.com>
2021-11-16 16:59:36 +0800

    Update 16

---
+++ src/16-my-pow/MyPow.cs
@@ -0,0 +1,44 @@
+﻿namespace CodingInterview;
+
+public class MyPow {
+    public static double Pow(double x, int n) {
+        var absExponent = n < 0 ? -n : n;
+
+        var result = PowUnsignedExponent(x, (uint)absExponent);
+        if (n < 0) {
+            result = 1.0 / result;
+        }
+
+        return result;
+    }
+
+    public static double PowUnsignedExponent(double num, uint exponent) {
+        var result = 1.0;
+        for (var i = 0; i < exponent; i++) {
+            result *= num;
+        }
+
+        return result;
+    }
+
+    public static double PowUnsignedExponentFast(double num, uint exponent) {
+        if (exponent == 0) {
+            return 1;
+        }
+        if (exponent == 1) {
+            return num;
+        }
+
+        // exponent / 2
+        var result = PowUnsignedExponentFast(num, exponent >> 1);
+
+        result *= result;
+
+        // exponent % 2 == 1
+        if ((exponent & 1) == 1) {
+            result *= num;
+        }
+
+        return result;
+    }
+}
